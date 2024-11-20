f9b630b7282d2bccb4e484dea164a3447715e974
Abhinav <abhinav2598@gmail.com>
2024-11-20 20:54:30 +0530

    added logger system design

---
+++ LoggerDesign/ErrorLogProcessor.java
@@ -0,0 +1,16 @@
+package com.SystemDesign.LoggerDesign;
+
+public class ErrorLogProcessor extends LogProcessor{
+
+    ErrorLogProcessor(LogProcessor logProcessor) {
+        super(logProcessor);
+    }
+    public void log(int loglevel,String message){
+        if(loglevel==ERROR){
+            System.out.println("ERROR "+message);
+        }
+        else{
+            super.log(loglevel,message);
+        }
+    }
+}
